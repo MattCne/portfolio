a91cd9e963cee550c1f68a2342efeed0fc417710
thanatos <thanatos@ubuntu.(none)>
2013-10-09 12:34:09 +0200

    ajout de la gestion des tailles des colonnes du footer

--- src/Mcneude/PortfolioBundle/Resources/public/js/script.js
+++ src/Mcneude/PortfolioBundle/Resources/public/js/script.js
@@ -1,5 +1,24 @@
+/**
+ * Gestion du slider
+ */
 $(function( $, undefined ){
     $('.flexslider').flexslider({
         animation: "slide",
     });
 });
+
+/**
+ * Gestion de la taille des colonnes dans le footer
+ */
+(function($, undefined){
+    var $sectionFooter = $('body #main > footer section');
+    var h = 0;
+    $sectionFooter.each(function(){
+        if($(this).outerHeight() >= h) h = $(this).outerHeight();
+    })
+    $sectionFooter.each(function(){
+        $(this).height(h);
+    })
+
+})(window.jQuery);
+
