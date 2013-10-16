80ce9407ae62670186e5d5bf5f3f9fc23a96f293
thanatos <thanatos@ubuntu.(none)>
2013-10-16 09:35:56 +0200

    config de base de l'admin

---
+++ src/Mcneude/PortfolioBundle/Admin/ProjetAdmin.php
@@ -0,0 +1,46 @@
+<?php
+namespace Mcneude\PortfolioBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class ProjetAdmin extends Admin
+{
+    /**
+     * Configuration du formulaire
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add( 'nom', 'text', array( 'label' => 'Nom du projet' ) )
+            ->add( 'accroche', 'text', array( 'label' => 'Accroche du projet' ) )
+        ;
+    }
+
+    /**
+     * Configuration des filtres
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('nom')
+            ->add('accroche')
+        ;
+    }
+
+    /**
+     * Configuration de la liste
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('nom')
+            ->add('accroche')
+        ;
+    }
+}
