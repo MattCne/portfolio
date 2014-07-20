3e14c72e09a5c5a111afde96fba6477c74d8d1ff
Matthieu Cneude <matthieu.cneude@gmail.com>
2014-07-20 20:51:49 +0200

    correction de quelques bugs et ajouts

--- src/Mcneude/PortfolioBundle/Formulaire/type/ContactType.php
+++ src/Mcneude/PortfolioBundle/Formulaire/type/ContactType.php
@@ -14,7 +14,7 @@ class ContactType extends AbstractType
             ->add('nom', 'text', array( 'required' => false ) )
             ->add('prenom', 'text', array( 'required' => false ) )
             ->add('sujet', 'text', array( 'attr' => array( 'class' => 'required' ) ) )
-            ->add('email', 'text', array( 'attr' => array( 'class' => 'required' ) ) )
+            ->add('email', 'email', array( 'attr' => array( 'class' => 'required' ) ) )
             ->add('societe', 'text', array( 'required' => false ) )
             ->add('adresse', 'text', array( 'required' => false ) )
             ->add('telephone', 'text', array( 'required' => false ) )
